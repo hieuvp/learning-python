4ef441d0ef74332398812ff97ca5fcfc56bb43ec
Harrison Van <harrison.van@shopback.com>
2020-07-18 18:15:42 +0700

    files

--- python-development-setup/virtualenv.sh
+++ python-development-setup/virtualenv.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
-set -eoux pipefail
+set -eou pipefail
 
+set -x
 type python
 type pip || true
 
 rm -rf venv
 virtualenv venv
+
+set +x
+echo "+ source venv/bin/activate"
 source venv/bin/activate
 
+set -x
 type python
 type pip
 
+pip list
 pip install --requirement requirements.txt
 pip list
 
