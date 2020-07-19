8fd43c88c11bf3c36d0bc84017ba485f19b66e9a
Harrison Van <harrison.van@shopback.com>
2020-07-19 12:05:08 +0700

    docs

--- learn-the-basics/lists/list.py
+++ learn-the-basics/lists/list.py
@@ -3,10 +3,10 @@ my_list.append(1)
 my_list.append(2)
 my_list.append(3)
 
-print(my_list[0])  # prints 1
-print(my_list[1])  # prints 2
-print(my_list[2])  # prints 3
+print(my_list[0])  # Print 1
+print(my_list[1])  # Print 2
+print(my_list[2])  # Print 3
 
-# Prints out 1,2,3
+# Print out 1, 2, 3
 for x in my_list:
     print(x)
