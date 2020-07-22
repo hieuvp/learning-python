b784609970783eaec73ce9d442eac0f8e7363397
Harrison Van <harrison.van@shopback.com>
2020-07-22 07:23:14 +0700

    cleanup

--- scripts/python.sh
+++ scripts/python.sh
@@ -2,16 +2,8 @@
 
 set -eoux pipefail
 
-# Must be >= 900 stars
-
 # Radon
 # https://github.com/rubik/radon
-
-# gjslint
-# https://developers.google.com/closure/utilities
-
-# Bandit 	Logical 	  Analyzes code to find common security issues
-# https://github.com/PyCQA/bandit
 
 # MyPy 	  Logical 	  Checks for optionally-enforced static types
 # http://mypy-lang.org/
