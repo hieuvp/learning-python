bf51c538a2bd8f854ac432c05288e53001278deb
Harrison Van <harrison.van@shopback.com>
2020-07-30 08:14:55 +0700

    fmt

--- learn-the-basics/basic-string-operations/string_slice.py
+++ learn-the-basics/basic-string-operations/string_slice.py
@@ -1,6 +1,6 @@
 A_STRING = "Hello world!"
 
-print("A_STRING[3:7] = %s" % A_STRING[3:7])
+print("A_STRING[3:7]   = %s" % A_STRING[3:7])
 
 # This prints a slice of the string, starting at index 3, and ending at index 6.
 # But why 6 and not 7? Again, most programming languages do this -
@@ -22,7 +22,7 @@ print("A_STRING[3:7:2] = %s" % A_STRING[3:7:2])
 # This prints the characters of string from 3 to 7 skipping one character.
 # This is extended slice syntax. The general form is `[start:stop:step]`.
 
-print("A_STRING[3:7] = %s" % A_STRING[3:7])
+print("A_STRING[3:7]   = %s" % A_STRING[3:7])
 print("A_STRING[3:7:1] = %s" % A_STRING[3:7:1])
 
 # Note that both of them produce same output
@@ -30,4 +30,4 @@ print("A_STRING[3:7:1] = %s" % A_STRING[3:7:1])
 # There is no function like strrev in C to reverse a string.
 # But with the above mentioned type of slice syntax you can easily reverse a string like this
 
-print("A_STRING[::-1] = %s" % A_STRING[::-1])
+print("A_STRING[::-1]  = %s" % A_STRING[::-1])
