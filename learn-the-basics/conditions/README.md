c527fd855c136f6421f18d3217d3d2a27c313097
Harrison Van <harrison.van@shopback.com>
2020-07-30 07:09:02 +0700

    fmt

--- learn-the-basics/conditions/README.md
+++ learn-the-basics/conditions/README.md
@@ -23,7 +23,6 @@ For example:
 
 ```py
 X = 2
-
 print(X == 2)
 print(X == 3)
 print(X < 3)
--- learn-the-basics/conditions/in_operator.py
+++ learn-the-basics/conditions/in_operator.py
@@ -1,3 +1,3 @@
-name = "John"
-if name in ["John", "Rick"]:
+NAME = "John"
+if NAME in ["John", "Rick"]:
     print("Your name is either John or Rick.")
