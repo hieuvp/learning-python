baf3a00798fa4293b377a660a0a4dbd501dbe9ec
Harrison Van <harrison.van@shopback.com>
2020-07-31 07:45:55 +0700

    docs

--- learn-the-basics/basic-string-operations/string_slice.py
+++ learn-the-basics/basic-string-operations/string_slice.py
@@ -1,31 +1,30 @@
 A_STRING = "Hello world!"
 
-# Starting at index "2"
-# Ending at index "6" (not "7")
+# Starting at index 2, and ending at index 6 (not 7)
 print("A_STRING[2:7]   = %s" % A_STRING[2:7])
 
 # If you just have one number in the brackets,
-# it will give you the single character at that index.
+# it will give you the single character at that index
+print("A_STRING[2]     = %s" % A_STRING[2])
+
 # If you leave out the first number but keep the colon,
-# it will give you a slice from the start to the number you left in.
+# it will give you a slice from the start to the number you left in
+print("A_STRING[:7]    = %s" % A_STRING[:7])
+
 # If you leave out the second number,
-# it will give you a slice from the first number to the end.
-#
+# it will give you a slice from the first number to the end
+print("A_STRING[2:]    = %s" % A_STRING[2:])
+
 # You can even put negative numbers inside the brackets.
 # They are an easy way of starting at the end of the string instead of the beginning.
 # This way, -3 means "3rd character from the end".
 
 print("A_STRING[2:7:2] = %s" % A_STRING[2:7:2])
 
-# This prints the characters of string from 3 to 7 skipping one character.
-# This is extended slice syntax. The general form is `[start:stop:step]`.
-
+# Print the characters of string from 2 to 7, skipping one character
+# Both of these produce same output
 print("A_STRING[2:7]   = %s" % A_STRING[2:7])
 print("A_STRING[2:7:1] = %s" % A_STRING[2:7:1])
 
-# Note that both of them produce same output
-#
-# There is no function like strrev in C to reverse a string.
-# But with the above mentioned type of slice syntax you can easily reverse a string like this
-
+# Easily reverse a string using slice
 print("A_STRING[::-1]  = %s" % A_STRING[::-1])
