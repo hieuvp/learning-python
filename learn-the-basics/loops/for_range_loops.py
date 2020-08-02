c78cc99d3a761d9b42aa37e33da7e8437026ac71
Harrison Van <harrison.van@shopback.com>
2020-08-02 08:36:55 +0700

    fmt

--- learn-the-basics/loops/for_range_loops.py
+++ learn-the-basics/loops/for_range_loops.py
@@ -1,14 +1,14 @@
 RESULT = "range(5)       ="
 for x in range(5):
     RESULT += " %d" % x
-print(RESULT)  # Print out the numbers 0,1,2,3,4
+print(RESULT)
 
 RESULT = "range(3, 6)    ="
 for x in range(3, 6):
     RESULT += " %d" % x
-print(RESULT)  # Print out 3,4,5
+print(RESULT)
 
 RESULT = "range(3, 8, 2) ="
 for x in range(3, 8, 2):
     RESULT += " %d" % x
-print(RESULT)  # Print out 3,5,7
+print(RESULT)
