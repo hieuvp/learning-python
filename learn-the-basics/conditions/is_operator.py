9aa1b03cb7425e3ed11b0dbf376fd58f706c3101
Harrison Van <harrison.van@shopback.com>
2020-08-03 06:50:10 +0700

    fmt

--- learn-the-basics/conditions/is_operator.py
+++ learn-the-basics/conditions/is_operator.py
@@ -3,6 +3,6 @@ y = [1, 2, 3]
 
 print(x == y)
 
-# Unlike the "double equals (==)" operator
+# Unlike the "double equals (==)" operator,
 # "is" operator does not match the "values" of the variables, but the "instances" themselves
 print(x is y)
