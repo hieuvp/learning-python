17b0356a96583ea35ee98ea6cb914ba2d246d131
Harrison Van <harrison.van@shopback.com>
2020-08-03 07:32:08 +0700

    cleanup

--- learn-the-basics/loops/exercise.py
+++ learn-the-basics/loops/exercise.py
@@ -1,7 +1,5 @@
 NUMBERS = [
-    386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345,
     399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217,
-    815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717,
     743, 527
 ]
 
@@ -10,6 +8,6 @@ for number in NUMBERS:
     if number % 2 == 0:
         print(number)
 
-    # Don't print any numbers that come after "237"
+    # Do not print any numbers that come after "237"
     if number == 237:
         break
