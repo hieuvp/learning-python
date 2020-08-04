d2b113b804e405af5ab7a9c81d8855aaa4da5188
Harrison Van <harrison.van@shopback.com>
2020-08-04 07:11:38 +0700

    ex

--- learn-the-basics/string-formatting/exercise.py
+++ learn-the-basics/string-formatting/exercise.py
@@ -1,6 +1,4 @@
-data = ("John", "Doe", 53.44)
-
-# A format string which prints out the data
 FORMAT_STRING = "Hello %s %s. Your current balance is $%.2f"
+DATA = ("John", "Doe", 53.44)
 
-print(FORMAT_STRING % data)
+print(FORMAT_STRING % DATA)
