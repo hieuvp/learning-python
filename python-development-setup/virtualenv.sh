f54285a4b7223910799df435243c1c6a77cd47a1
Harrison Van <harrison.van@shopback.com>
2020-08-04 09:38:20 +0700

    cleanup

--- python-development-setup/virtualenv.sh
+++ python-development-setup/virtualenv.sh
@@ -2,20 +2,13 @@
 
 set -eou pipefail
 
-<<<<<<< HEAD
-=======
 # Cleaning up
 rm -rf venv
 
->>>>>>> master
 set -x
 type python
 type pip || true
 
-<<<<<<< HEAD
-rm -rf venv
-=======
->>>>>>> master
 virtualenv venv
 
 set +x
@@ -26,17 +19,12 @@ set -x
 type python
 type pip
 
-<<<<<<< HEAD
-pip list
-pip install --requirement requirements.txt
-=======
 pip install --quiet --upgrade pip
 pip list
 pip freeze
 
 pip install --quiet --requirement requirements.txt
 pip list
->>>>>>> master
 pip freeze
 
 rm -rf venv
