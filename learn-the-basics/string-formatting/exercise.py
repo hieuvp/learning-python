279b3ec7cb2c91682b52f6fca5ace203ec0462d8
Harrison Van <harrison.van@shopback.com>
2020-08-04 07:23:02 +0700

    docs: python

---
+++ learn-the-basics/string-formatting/exercise.py
@@ -0,0 +1,4 @@
+FORMAT_STRING = "Hello %s %s. Your current balance is $%.2f"
+DATA = ("John", "Doe", 53.44)
+
+print(FORMAT_STRING % DATA)
---
+++ learn-the-basics/string-formatting/print_formatted_object.py
@@ -0,0 +1,4 @@
+MY_LIST = [1, 2, 3]
+
+print("repr(MY_LIST) = %s" % repr(MY_LIST))
+print("MY_LIST       = %s" % MY_LIST)
