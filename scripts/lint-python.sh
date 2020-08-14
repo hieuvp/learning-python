825e643aeec81e20d0dfd277a14f7119b36b05c7
Harrison Van <harrison.van@shopback.com>
2020-08-14 08:30:11 +0700

    ignore case

--- scripts/lint-python.sh
+++ scripts/lint-python.sh
@@ -15,6 +15,7 @@ declare -ra IGNORING_FILES=(
   "learn-the-basics/conditions/in_operator.py"
   "learn-the-basics/conditions/not_operator.py"
   "learn-the-basics/functions/call_functions.py"
+  "learn-the-basics/functions/write_functions.py"
   "learn-the-basics/loops/break_continue_statements.py"
   "learn-the-basics/loops/else_clause.py"
   "learn-the-basics/loops/for_range_loops.py"
