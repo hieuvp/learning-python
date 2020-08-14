93b2111fe796f48003dc7d0e0ef186e2ace3a39b
Harrison Van <harrison.van@shopback.com>
2020-08-14 07:55:53 +0700

    reset

--- learn-the-basics/modules-and-packages/customize_import_name/draw_textual.py
+++
@@ -1,5 +0,0 @@
-# Source: custom_import_name/draw.py
-
-
-def draw_game(result):
-    print('draw_game("%s") in textual mode' % result)
--- learn-the-basics/modules-and-packages/my_game/draw.py
+++
@@ -1,9 +0,0 @@
-# Source: my_game/draw.py
-
-
-def draw_game(result):
-    print('draw_game("%s")' % result)
-
-
-def clear_screen(screen):
-    print('clear_screen("%s")' % screen)
