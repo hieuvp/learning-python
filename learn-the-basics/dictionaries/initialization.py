3304fce1ee3a2df396c7e591cfd1ad8f6a4dafe0
Harrison Van <harrison.van@shopback.com>
2020-08-19 07:28:36 +0700

    fmt

--- learn-the-basics/dictionaries/initialization.py
+++ learn-the-basics/dictionaries/initialization.py
@@ -1,12 +1,11 @@
-# For example, a database of phone numbers could be stored using a dictionary like this:
-
+# A database of phone numbers could be stored using a dictionary
 phonebook = {}
 phonebook["John"] = 938477566
 phonebook["Jack"] = 938377264
 phonebook["Jill"] = 947662781
 print(phonebook)
 
-# Alternatively, a dictionary can be initialized with the same values in the following notation:
-
+# Alternatively,
+# a dictionary can be initialized with the same values in the following notation
 phonebook = {"John": 938477566, "Jack": 938377264, "Jill": 947662781}
 print(phonebook)
