a821514f7dd52be8a744ee2414184471abafd2cb
Harrison Van <harrison.van@shopback.com>
2020-08-19 08:13:44 +0700

    docs: classes, objects, and dictionaries exercises (#17)

---
+++ learn-the-basics/dictionaries/initialization.py
@@ -0,0 +1,11 @@
+# A database of phone numbers could be stored using a dictionary
+phonebook = {}
+phonebook["John"] = 938477566
+phonebook["Jack"] = 938377264
+phonebook["Jill"] = 947662781
+print(phonebook)
+
+# Alternatively,
+# a dictionary can be initialized with the same values in the following notation
+phonebook = {"John": 938477566, "Jack": 938377264, "Jill": 947662781}
+print(phonebook)
--- scripts/lint-python.sh
+++ scripts/lint-python.sh
@@ -14,10 +14,13 @@ declare -ra IGNORING_FILES=(
   "learn-the-basics/conditions/boolean_operators.py"
   "learn-the-basics/conditions/in_operator.py"
   "learn-the-basics/conditions/not_operator.py"
+  "learn-the-basics/functions/call_functions.py"
+  "learn-the-basics/functions/write_functions.py"
   "learn-the-basics/loops/break_continue_statements.py"
   "learn-the-basics/loops/else_clause.py"
   "learn-the-basics/loops/for_range_loops.py"
   "learn-the-basics/loops/while_loop.py"
+  "learn-the-basics/modules-and-packages/import_all_module_objects/game.py"
 )
 
 main() {
