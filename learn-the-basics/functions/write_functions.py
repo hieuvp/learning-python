2367659936b2e36f39478d8dbc7207f0e55f9c86
Harrison Van <harrison.van@shopback.com>
2020-08-20 07:24:17 +0700

    fmt

--- learn-the-basics/functions/write_functions.py
+++ learn-the-basics/functions/write_functions.py
@@ -1,13 +1,13 @@
 # Functions are defined using the block keyword "def",
 # followed with the function's name as the block's name
 def my_function():
-    print("Hello From My Function!")
+    print("Hello, From My Function!")
 
 
 # Functions may also receive arguments
 # (variables passed from the caller to the function)
 def my_function_with_args(username, greeting):
-    print("Hello, %s , From My Function!, I wish you %s" % (username, greeting))
+    print("Hello, %s, From My Function! I wish you %s" % (username, greeting))
 
 
 # Functions may return a value to the caller,
