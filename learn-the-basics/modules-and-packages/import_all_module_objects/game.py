32052f04652e1b891aae94eabe208f5c4b7a0b25
Harrison Van <harrison.van@shopback.com>
2020-08-23 09:10:09 +0700

    cleanup

--- learn-the-basics/modules-and-packages/import_all_module_objects/game.py
+++ learn-the-basics/modules-and-packages/import_all_module_objects/game.py
@@ -5,13 +5,8 @@
 from draw import *
 
 
-def play_game():
-    return "ping pong"
-
-
 def main():
-    name = play_game()
-    draw_game(name)
+    draw_game("ping pong")
 
 
 if __name__ == "__main__":
