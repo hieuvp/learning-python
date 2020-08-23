b182c2ecbb770bd4b4d2e4c6a67090fcd9be47aa
Harrison Van <harrison.van@shopback.com>
2020-08-23 10:51:59 +0700

    reset

--- learn-the-basics/modules-and-packages/write_package/pkg/draw.py
+++
@@ -1,9 +0,0 @@
-# Source: write_package/pkg/draw.py
-
-
-def draw_game(name):
-    print('draw_game("%s")' % name)
-
-
-def clear_screen(game):
-    print('clear_screen("%s")' % game)
--- name-variable/labs/scenario-2/importing_script.py
+++
@@ -1,8 +0,0 @@
-# Source: scenario-2/importing_script.py
-
-import stand_alone_script as sas
-
-
-print('The value of "__name__" is "%s"' % __name__)
-
-sas.my_function()
