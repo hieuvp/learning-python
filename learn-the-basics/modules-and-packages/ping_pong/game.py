e94a5c050201a8b2b1f32a3fbaf5746e816b7a1c
Harrison Van <harrison.van@shopback.com>
2020-08-25 09:00:01 +0700

    reset

--- learn-the-basics/modules-and-packages/ping_pong/game.py
+++ learn-the-basics/modules-and-packages/ping_pong/game.py
@@ -1,20 +1,8 @@
 # Source: ping_pong/game.py
-# This "game" module is responsible for the game logic
+# This module is responsible for the game logic
 
-
-# Modules are imported from other modules using the "import" command.
-# In this example, this "game" module imports the "draw" module
-# which enables it to use functions implemented in that module.
+# Importing the "draw" module
 import draw
-
-
-# When the "import draw" directive run,
-# the Python interpreter will look for a file in the directory
-# which the script was executed from,
-# by the name of the module with a ".py" suffix,
-# so in our case it will try to look for "draw.py".
-# If it will find one, it will import it.
-# If not, it will continue to look for built-in modules.
 
 
 def play_game():
@@ -22,21 +10,8 @@ def play_game():
 
 
 def main():
-    # The main function would use the local function "play_game" to run the game,
-    # and then draw the result of the game
-    # using a function implemented in the draw module called "draw_game".
-    # To use the function "draw_game" from the "draw" module,
-    # we would need to specify in which module the function is implemented,
-    # using the dot operator.
-    # To reference the "draw_game" function from the "game" module,
-    # we would need to import the "draw" module and only then call "draw.draw_game()".
     name = play_game()
-
-    # Using the function "draw_game"
-    # from the file "draw.py", or in other words, the "draw" module,
-    # that implements the logic for drawing the game on the screen.
     draw.draw_game(name)
-
     draw.clear_screen(name)
 
 
