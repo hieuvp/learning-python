d7c8ca8b06a7fbd53eb5e38e2389700e67b5f488
Harrison Van <harrison.van@shopback.com>
2020-08-25 08:17:07 +0700

    fmt

--- learn-the-basics/modules-and-packages/extend_module_search_path.sh
+++ learn-the-basics/modules-and-packages/extend_module_search_path.sh
@@ -8,7 +8,6 @@ set +x
 echo
 set -x
 
-# Enable this script to load modules
-# from ".." (means "./learning-python/learn-the-basics") directory
-# as well as the local directory
+# Enabling the Python script to load modules
+# from ".." (meaning: "/learning-python/learn-the-basics") directory
 PYTHONPATH=".." python extend_module_search_path.py
