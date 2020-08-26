b40f076391037b8553e64031a006676569b6d53b
Harrison Van <harrison.van@shopback.com>
2020-08-27 06:53:01 +0700

    reset

--- data-science/numpy-arrays/subset.py
+++
@@ -1,7 +0,0 @@
-from calculate_bmi import bmi
-
-# For a boolean response
-print("(bmi > 23) = %s" % (bmi > 23))
-
-# Print only those observations above 23
-print("bmi[bmi > 23] = %s" % bmi[bmi > 23])
