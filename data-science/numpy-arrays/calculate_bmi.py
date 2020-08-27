ef8aa44bdbce8e14a1b433e90981850b1483626f
Harrison Van <harrison.van@shopback.com>
2020-08-27 09:33:50 +0700

    reset

--- data-science/numpy-arrays/calculate_bmi.py
+++
@@ -1,8 +0,0 @@
-from getting_started import np_weight, np_height
-
-# Calculate bmi
-bmi = np_weight / np_height ** 2
-
-if __name__ == "__main__":
-    # Print the result
-    print("bmi = %s" % bmi)
