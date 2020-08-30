af8f2603246729aa47cc28b9db8d0d92ba157e0b
Harrison Van <harrison.van@shopback.com>
2020-08-30 11:26:41 +0700

    reset

--- data-science/pandas-basics/read_csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Now, the csv `cars.csv` is stored and can be imported using `pd.read_csv`:
-# Import the "cars.csv" data
-cars = pd.read_csv("cars.csv")
-
-# Print out cars
-print(cars)
