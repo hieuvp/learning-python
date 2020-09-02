50fcd3035540a07536928872878e2da4745a4984
Harrison Van <harrison.van@shopback.com>
2020-09-02 08:39:04 +0700

    fmt

--- advanced-tutorials/decorators/exercise.py
+++ advanced-tutorials/decorators/exercise.py
@@ -1,3 +1,12 @@
+# Make a decorator factory which returns a decorator that decorates functions with one argument.
+# The factory should take one argument, a type,
+# and then returns a decorator that makes function should check if the input is the correct type.
+# If it is wrong, it should print("Bad Type") (In reality, it should raise an error,
+# but error raising isn't in this tutorial).
+# Look at the tutorial code and expected output to see
+# what it is if you are confused (I know I would be.)
+# Using `isinstance(object, type_of_object)` or `type(object)` might help.
+
 # def type_check(correct_type):
 #     # put code here
 #     pass
