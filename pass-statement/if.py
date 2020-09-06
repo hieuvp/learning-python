b66971fcd1420576ff562f92073fe677d29496ee
Harrison Van <harrison.van@shopback.com>
2020-09-06 10:35:00 +0700

    reset

--- pass-statement/if.py
+++
@@ -1,7 +0,0 @@
-# Using the `pass` keyword in an `if` statement:
-
-a = 33
-b = 200
-
-if b > a:
-    pass
--- pass-statement/loop.py
+++
@@ -1,4 +0,0 @@
-# Using the `pass` keyword in a `for` loop
-
-for x in [0, 1, 2]:
-    pass
