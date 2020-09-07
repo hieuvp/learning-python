5402bb85917153faa475e2b86947f61c38aa6351
Harrison Van <harrison.van@shopback.com>
2020-09-07 08:30:59 +0700

    resolved

--- scripts/lint-python.sh
+++ scripts/lint-python.sh
@@ -36,10 +36,7 @@ declare -ra IGNORING_FILES=(
   "learn-the-basics/loops/while_loop.py"
   "learn-the-basics/modules-and-packages/import_all_module_objects/game.py"
   "learn-the-basics/modules-and-packages/initialize_module/draw.py"
-<<<<<<< HEAD
   "pass-statement/example.py"
-=======
->>>>>>> master
   "pass-statement/if.py"
 )
 
