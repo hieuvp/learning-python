af5f78d14d1a0cafcfe5f57d11ce1a3f8bc0943c
Harrison Van <harrison.van@shopback.com>
2020-09-12 10:44:15 +0700

    fmt

--- advanced-tutorials/decorators/repeater.py
+++ advanced-tutorials/decorators/repeater.py
@@ -1,16 +1,17 @@
-# This would make a function repeat twice
+# Making a function repeat twice
 def repeater(old_function):
     def new_function(*args, **kwargs):
-        old_function(*args, **kwargs)  # we run the old function
-        old_function(*args, **kwargs)  # we do it twice
+        old_function(*args, **kwargs)  # Run the "old_function"
+        old_function(*args, **kwargs)  # Do it twice
 
-    # Return the new_function, or it wouldn't reassign it to the value
+    # Return the "new_function",
+    # or it wouldn't reassign it to the value
     return new_function
 
 
 @repeater
-def multiply(num1, num2):
-    print(num1 * num2)
+def multiply(multiplier, multiplicand):
+    print(multiplier * multiplicand)
 
 
 multiply(2, 3)
