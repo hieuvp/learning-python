54ee37196fe1c535bc1f707fb638c1e056b8f3b0
Harrison Van <harrison.van@shopback.com>
2020-09-15 08:11:50 +0700

    ex

--- advanced-tutorials/regular-expressions/exercise.py
+++ advanced-tutorials/regular-expressions/exercise.py
@@ -1,27 +1,27 @@
-# Example:
 import re
 
-pattern = re.compile(r"\[(on|off)\]")  # Slight optimization
-print(re.search(pattern, "Mono: Playback 65 [75%] [-16.50dB] [on]"))
+# Slight optimization
+pattern = re.compile(r"\[(on|off)\]")
+
 # Returns a Match object!
+print(re.search(pattern, "Mono: Playback 65 [75%] [-16.50dB] [on]"))
+
+# Doesn't return anything
 print(re.search(pattern, "Nada...:-("))
-# Doesn't return anything.
-# End Example
 
 
-# Exercise: make a regular expression that will match an email
 def test_email(your_pattern):
     pattern = re.compile(your_pattern)
     emails = ["john@example.com", "python-list@python.org", "wha.t.`1an?ug{}ly@email.com"]
     for email in emails:
         if not re.match(pattern, email):
-            print("You failed to match %s" % (email))
+            print("You failed to match %s" % email)
         elif not your_pattern:
             print("Forgot to enter a pattern!")
         else:
             print("Pass")
 
 
-# Your pattern here!
-pattern = r""
+# Making a regular expression that will match an email
+pattern = r"[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}"
 test_email(pattern)
