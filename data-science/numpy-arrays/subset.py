93af63a1e7ae7fd262e1b168af7a913311b75574
Harrison Van <harrison.van@shopback.com>
2020-09-18 08:09:12 +0700

    docs

--- data-science/numpy-arrays/subset.py
+++ data-science/numpy-arrays/subset.py
@@ -2,12 +2,9 @@ from bmi import bmi
 
 print("bmi           = %s" % bmi)
 
-# For instance,
-# if you wanted to know which observations in our BMI array are above 25,
+# If we wanted to know which observations in our BMI array are above "25",
 # we could quickly subset it to find out
-
-# Boolean response
 print("(bmi > 25)    = %s" % (bmi > 25))
 
-# Print only those observations above 25
+# Print only those observations above "25"
 print("bmi[bmi > 25] = %s" % bmi[bmi > 25])
