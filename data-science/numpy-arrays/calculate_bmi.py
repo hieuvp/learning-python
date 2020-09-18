a388c38ba27dc421ef2d8d40d78f39c8c65ed486
Harrison Van <harrison.van@shopback.com>
2020-09-18 07:24:18 +0700

    docs

--- data-science/numpy-arrays/calculate_bmi.py
+++ data-science/numpy-arrays/calculate_bmi.py
@@ -7,16 +7,14 @@ from getting_started import np_height, np_weight
 # - kg  : a person's weight in kilograms
 # - m^2 : their height in metres squared
 #
-# A BMI of 25.0 or more is overweight, while the healthy range is 18.5 to 24.9.
-# BMI applies to most adults 18-65 years.
+# A BMI of "25.0" or more is overweight, while the healthy range is "18.5" to "24.9"
+# BMI applies to most adults 18-65 years
+
 
 # Now we can perform element-wise calculations on height and weight.
-#
 # For example,
 # you could take all 6 of the height and weight observations above,
 # and calculate the BMI (Body Mass Index) for each observation with a single equation.
-
-
 bmi = np_weight / np_height ** 2
 
 # These operations are very fast and computationally efficient.
