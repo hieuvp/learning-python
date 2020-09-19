0806076c1a1366768865ce76af84a82d597e36cb
Harrison Van <harrison.van@shopback.com>
2020-09-19 13:29:40 +0700

    reset

--- advanced-tutorials/sets/symmetric_difference.py
+++
@@ -1,6 +0,0 @@
-a = set(["Jake", "John", "Eric"])
-b = set(["John", "Jill"])
-
-# To find out which members attended only one of the events
-print(a.symmetric_difference(b))
-print(b.symmetric_difference(a))
--- data-science/pandas-basics/read_csv.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Importing the "cars.csv" data
-cars = pd.read_csv("cars.csv")
-
-# Print out cars
-print(cars)
