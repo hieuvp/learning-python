3a90a304c20ee860f39ae6cdddf6cdd90a6d8e6c
Harrison Van <harrison.van@shopback.com>
2020-09-19 11:12:16 +0700

    docs

--- advanced-tutorials/closures/transmit_to_space.py
+++ advanced-tutorials/closures/transmit_to_space.py
@@ -3,12 +3,12 @@ def transmit_to_space(message):
 
     def data_transmitter():
         """This is the Nested Function"""
-        """It is a function defined inside another function"""
+        """It is a Function defined inside another Function"""
 
         print(message)
-        """Nested functions can access the variables of the enclosing scope"""
+        """Nested Functions can access the variables of the enclosing scope"""
         """These variables are readonly"""
-        """However, one can use the "nonlocal" keyword explicitly in order to modify them"""
+        """However, one can use the nonlocal keyword explicitly in order to modify them"""
 
     data_transmitter()
 
