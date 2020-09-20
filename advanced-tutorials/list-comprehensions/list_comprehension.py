4db4cefa77bf90474b7332c4a169a2f9152567a5
Harrison Van <harrison.van@shopback.com>
2020-09-20 11:32:13 +0700

    docs

--- advanced-tutorials/list-comprehensions/list_comprehension.py
+++ advanced-tutorials/list-comprehensions/list_comprehension.py
@@ -1,20 +1,18 @@
-SENTENCE = "the quick brown fox jumps over the lazy dog"
-WORDS = SENTENCE.split()
+WORDS = "the quick brown fox jumps over the lazy dog".split()
 
 
-# Create a list of integers
-# which specify the length of each word in a certain sentence,
+# Create a list of integers which specify the length of each word in a certain sentence,
 # but only if the word is not the word "the"
-word_lengths = []
+lengths = []
 for word in WORDS:
     if word != "the":
-        word_lengths.append(len(word))
+        lengths.append(len(word))
 print(WORDS)
-print(word_lengths)
+print(lengths)
 
 print()
 
 # When using a "List Comprehension", we could simplify this process
-word_lengths = [len(word) for word in WORDS if word != "the"]
+lengths = [len(word) for word in WORDS if word != "the"]
 print(WORDS)
-print(word_lengths)
+print(lengths)
