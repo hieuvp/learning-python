e42660c5ef282cfe8a8e1eedb5b37759d8d57c99
Harrison Van <harrison.van@shopback.com>
2020-09-21 07:39:58 +0700

    docs

--- advanced-tutorials/list-comprehensions/exercise.py
+++ advanced-tutorials/list-comprehensions/exercise.py
@@ -1,8 +1,7 @@
+# Use a "list comprehension"
+# to create a list of "positive_integers" out of "numbers"
 numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
-print("numbers           = %s" % numbers)
-
-
-# Using a "List Comprehension"
-# to create a list of "positive_integers" out of the "numbers" list
 positive_integers = [int(number) for number in numbers if number > 0]
+
+print("numbers           = %s" % numbers)
 print("positive_integers = %s" % positive_integers)
