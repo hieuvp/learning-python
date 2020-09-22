923e0077814f0ff4ab283a5807840fe135868181
Harrison Van <harrison.van@shopback.com>
2020-09-22 08:57:42 +0700

    chore: pass and lambda exercises (#31)

---
+++ lambda-functions/sum.py
@@ -0,0 +1,3 @@
+# A lambda function that sums arguments a, b, and c
+sum = lambda a, b, c: a + b + c
+print(sum(3, 4, 5))
---
+++ pass-statement/example.py
@@ -0,0 +1,6 @@
+for letter in "Python":
+    if letter == "h":
+        pass
+        print('This is the "pass" block')
+
+    print("Current letter: %s" % letter)
