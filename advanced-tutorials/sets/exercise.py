963c4ff84efb8f170237afe012b1313044feb9e5
Harrison Van <harrison.van@shopback.com>
2020-09-27 12:49:24 +0700

    reset

--- advanced-tutorials/sets/exercise.py
+++
@@ -1,6 +0,0 @@
-a = ["Jake", "John", "Eric"]
-b = ["John", "Jill"]
-
-# Print out a "set" containing all the participants
-# from the event A who did not attend the event B
-print(set(a).difference(set(b)))
