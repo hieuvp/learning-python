f0a326934a7a71c0dcce324f637116f7f5965021
Harrison Van <harrison.van@shopback.com>
2020-09-28 07:37:36 +0700

    docs

--- advanced-tutorials/serialization/exercise.py
+++ advanced-tutorials/serialization/exercise.py
@@ -14,7 +14,7 @@ def add_employee(salaries_json, name, salary):
 def main():
     salaries_json = '{"Alfred" : 300, "Jane" : 400 }'
 
-    # Add new key-value pair
+    # Add a new key-value pair
     salaries_json = add_employee(salaries_json, "Me", 800)
 
     # Print out the values
