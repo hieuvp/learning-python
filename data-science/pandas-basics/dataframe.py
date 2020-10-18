459506b6bdea9911b0767c37351e5971be6a451d
Harrison Van <harrison.van@shopback.com>
2020-10-18 10:59:17 +0700

    docs: learn python

--- data-science/pandas-basics/dataframe.py
+++ data-science/pandas-basics/dataframe.py
@@ -3,7 +3,7 @@ import pandas as pd
 # Use a Dictionary
 dictionary = {
     "country": ["Brazil", "Russia", "India", "China", "South Africa"],
-    "capital": ["Brasilia", "Moscow", "New Dehli", "Beijing", "Pretoria"],
+    "capital": ["Brasilia", "Moscow", "New Delhi", "Beijing", "Pretoria"],
     "area": [8.516, 17.10, 3.286, 9.597, 1.221],
     "population": [200.4, 143.5, 1252, 1357, 52.98],
 }
